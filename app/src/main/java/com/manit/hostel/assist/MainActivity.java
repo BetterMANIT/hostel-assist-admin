a46f80e09666a401174a140816fda9c5a8692ad0
Rock26soft <74952168+Rock26soft@users.noreply.github.com>
2024-09-22 16:05:32 +0530

    mainactivity

--- app/src/main/java/com/manit/hostel/assist/MainActivity.java
+++ app/src/main/java/com/manit/hostel/assist/MainActivity.java
@@ -3,17 +3,23 @@ package com.manit.hostel.assist;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.manit.hostel.assist.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-    @NonNull ActivityMainBinding lb;
+    ActivityMainBinding lb;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         lb = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(lb.getRoot());
+        lb.getRoot().postDelayed(()->{
+            Intent in = new Intent();
+            in.setClass(getApplicationContext(),LoginActivity.class);
+            startActivity(in);
+        },2000);
     }
 }
